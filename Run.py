b0748bd5c6b8bd1f081b3bbc9e2334035816ed7e
刘子健_LiuZijian <alphatk@vip.qq.com>
2024-03-22 15:24:03 +0800

    Update file Run.py

--- Run.py
+++ Run.py
@@ -2,9 +2,13 @@ import argparse
 
 if __name__ == '__main__':
 
-
-
     parser = argparse.ArgumentParser()  # Create the argument parser
 
     # Device:
-    parser.add_argument('')
+    parser.add_argument('--device', type=str, default='cuda:0',help='cuda or cpu')
+
+
+
+
+
+    args = parser.parse_args()
