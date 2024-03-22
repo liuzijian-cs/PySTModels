75aa84075b404d870fa51dd0cc43d0da027db7dd
刘子健_LiuZijian <alphatk@vip.qq.com>
2024-03-22 17:14:48 +0800

    Update file Run.py (240322)

--- Run.py
+++ Run.py
@@ -1,14 +1,30 @@
 import argparse
 
+import torch
+
 if __name__ == '__main__':
 
+    # 1. Parser
     parser = argparse.ArgumentParser()  # Create the argument parser
 
-    # Device:
-    parser.add_argument('--device', type=str, default='cuda:0',help='cuda or cpu')
+    # 1.1 Basic config:
+    parser.add_argument('--device', type=str, default='cuda:0', help='cuda or cpu')
+    parser.add_argument('--model', type=str, default='iTransformer', required=True,
+                        help='model list: [iTransformer]')
+
+    # 1.2 Model arguments:
 
 
-
-
+    # 1.2
+    # 1.2 Model:
+    ## Common Parameter:
 
     args = parser.parse_args()
+
+    # init:
+
+    # Device:
+    if args.device == 'cuda':
+        if not torch.cuda.is_available():
+            print('No GPU found')
+            args.device = 'cpu'
