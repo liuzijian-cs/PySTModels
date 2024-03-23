9dada10319327b50a628c60071f6932e2f09e870
刘子健_LiuZijian <alphatk@vip.qq.com>
2024-03-23 11:11:33 +0800

    Update file Run.py (240323)

--- Run.py
+++ Run.py
@@ -1,6 +1,7 @@
 import argparse
 
 import torch
+import numpy as np
 
 if __name__ == '__main__':
 
@@ -11,20 +12,30 @@ if __name__ == '__main__':
     parser.add_argument('--device', type=str, default='cuda:0', help='cuda or cpu')
     parser.add_argument('--model', type=str, default='iTransformer', required=True,
                         help='model list: [iTransformer]')
+    parser.add_argument('--seed', type=int, default=None, help='random seed')
 
     # 1.2 Model arguments:
 
-
     # 1.2
     # 1.2 Model:
-    ## Common Parameter:
+    # Common Parameter:
 
     args = parser.parse_args()
 
-    # init:
+    # Initialization:
 
-    # Device:
+    # Device initialization:
     if args.device == 'cuda':
         if not torch.cuda.is_available():
-            print('No GPU found')
-            args.device = 'cpu'
+            raise RuntimeError(f'cuda is not available! (args.device == {args.deivce})')
+    # Seed initialization:
+    if args.seed is not None:
+        np.random.seed(args.seed)
+        torch.manual_seed(args.seed)
+        if args.device == 'cuda' and torch.cuda.is_available():
+            torch.cuda.init()
+            torch.cuda.manual_seed_all(args.seed)
+            torch.backends.cudnn.deterministic = True
+            torch.backends.cudnn.benchmark = False
+
+
