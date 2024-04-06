e3720ab6c63ec1c9b2c08958610487f33d11208e
刘子健_LiuZijian <alphatk@vip.qq.com>
2024-04-06 20:13:17 +0800

    Update(240403): Trainer coding中

--- utils/taskMaker/TimeSeriesForecast.py
+++ utils/taskMaker/TimeSeriesForecast.py
@@ -33,14 +33,17 @@ class Task(BasicTask):
     def _inverse_scale(self, y_pred, y_true) -> np.array:
         return self.DataProvider.inverse_transform(y_pred), self.DataProvider.inverse_transform(y_true)
 
+    # def valid(self):
+    #
+
     def train(self):
         t1 = time.time()
         print_log(self.log_file, f"{Color.G}>>> Start training.{Color.RE}")
         iter_count = 0
         for epoch in range(self.args.epochs):
             self.model.train()
+            iter_loss, iter_mae, iter_rmse, iter_mape = [], [], [], []
             for batch_idx, (batch_x, y_true) in enumerate(self.train_loader):
-                time_itor = time.time()
                 self.model_optimizer.zero_grad()
                 batch_x, y_true = batch_x.to(self.device), y_true.to(self.device)
 
@@ -63,16 +66,41 @@ class Task(BasicTask):
                 mape = self._mape(y_pred, y_true)
 
                 if self.args.data_inverse_scale:
-                    y_pred = torch.tensor(self.DataProvider.inverse_transform(y_pred)).to(self.device)
-                    y_true = torch.tensor(self.DataProvider.inverse_transform(y_true)).to(self.device)
-                    mae_ = self._mae(y_pred, y_true)
-                    rmse_ = self._rmse(y_pred, y_true)
-                    mape_ = self._mape(y_pred, y_true)
+                    y_pred = self.DataProvider.inverse_transform(y_pred)
+                    y_true = self.DataProvider.inverse_transform(y_true)
+                    mae_inverse_scale = self._mae(y_pred, y_true)
+                    rmse_inverse_scale = self._rmse(y_pred, y_true)
+                    mape_inverse_scale = self._mape(y_pred, y_true)
 
                 if self.args.log_interval_iter and batch_idx % self.args.log_interval_iter == 0:
                     print_log(self.log_file,
-                              f"{Color.P}epoch{epoch:03d}-{batch_idx:03d} ({(time.time()-time_itor):4.2f}s/iter & {((time.time()-time_itor)*len(self.test_loader)):4.2f}s/epoch){Color.RE}: loss:{loss_item:.4f}, mae:{mae:.4f}({mae_:.4f}), rmse:{rmse:.4f}({rmse_:.4f}), mape:{mape:.4f}({mape_:.4f})")
+                              f"{Color.P}epoch{epoch:03d}-{batch_idx:03d} {Color.RE}: loss:{loss_item:.4f}, mae:{mae_inverse_scale:.4f}({mae:.4f}), rmse:{rmse_inverse_scale:.4f}({rmse:.4f}), mape:{mape_inverse_scale:.4f}({mape:.4f})")
                 iter_count += 1
+                # Save iter record
+                iter_loss.append(loss_item)
+                iter_mae.append(mae)
+                iter_rmse.append(rmse)
+                iter_mape.append(mape)
+                # BATCH for END
+
+            # Record:
+            epoch_loss = np.average(iter_loss)
+            epoch_mae = np.average(iter_mae)
+            epoch_rmse = np.average(iter_rmse)
+            epoch_mape = np.average(iter_mape)
+            epoch_mae_inverse = self.DataProvider.inverse_transform(epoch_mae)  # ? 不知能不能用
+            epoch_rmse_inverse = self.DataProvider.inverse_transform(epoch_rmse)
+            epoch_mape_inverse = self.DataProvider.inverse_transform(epoch_mape)
+            print(f"{epoch_loss},{epoch_mae},{epoch_rmse},{epoch_mape},{epoch_mae_inverse},{epoch_rmse_inverse},{epoch_mape_inverse}")
+
+            self.record['train']['loss'].append(loss_item)
+            self.record['train']['mae'].append(mae)
+            self.record['train']['rmse'].append(rmse)
+            self.record['train']['mape'].append(mape)
+            self.record['train']['mae_'].append(mae_inverse_scale)
+            self.record['train']['rmse_'].append(rmse_inverse_scale)
+            self.record['train']['mape_'].append(mape_inverse_scale)
+            # VALID:
 
 #
 #
